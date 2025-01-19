eca66e78b41468ba2bef77e9785d7834de243f33
Banjoman221 <jedijacob11@gmail.com>
2025-01-19 10:25:43 -0600

    updated powershell script

--- updater.ps1
+++ updater.ps1
@@ -7,30 +7,36 @@ rm .\HymnOS\ -r -Force
 $currentDirectory = pwd
 cd $currentDirectory
 
-git add .
+$continueing = Read-Host "Would you like to commit changes? enter c for commit or press enter to pull changes(default)"
 
-git status
-$gitStatus = git status
-#Read-Host $gitStatus
+if($continueing -ne 'c'){
+ git pull
+}elseif ($continueing -eq 'c') {
+  git add .
 
-if($gitStatus -like "*changes*"){
-  $commitMessage = Read-Host "Please enter a commit message or enter 'c' to continue" 
-  if($commitMessage -ne 'c'){
-    git commit -m $commitMessage
+  git status
 
-    git push
+  $gitStatus = git status
+
+  if($gitStatus -like "*changes*"){
+    $commitMessage = Read-Host "Please enter a commit message or enter 'c' to continue" 
+    if($commitMessage -ne 'c'){
+      git commit -m $commitMessage
+
+      git push
+    }
   }
-}
-Start-Sleep -seconds 5
+  Start-Sleep -seconds 5
 
-$updating = git pull
+  $updating = git pull
 
-if($updating -like '* up to date*'){
-  Write-Host "App is already up to date"
-}elseif(err){
-  Write-Host "Updated has failed"
-}else{
-  Write-Host "App has been updated successfully"
+  if($updating -like '* up to date*'){
+    Write-Host "App is already up to date"
+  }elseif(err){
+    Write-Host "Updated has failed"
+  }else{
+    Write-Host "App has been updated successfully"
+  }
 }
 
 Start-Sleep -seconds 5
